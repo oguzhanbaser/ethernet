f41ebc4d15628ed3620a11ee384db52f7085feb7
Oğuzhan Başer <baser61061@gmail.com>
2023-05-29 00:51:42 +0300

    esp pico mqtt

---
+++ MqttProject/Readme.md
@@ -0,0 +1,9 @@
+# MQTT Example
+
+### Project details
+- Raspberry Pi Pico and ESP32 used in this project
+- shiftr.io used as MQTT Broker
+- MQTT Explorer used in this project to show published values and publis values to devices
+
+### Wiring Diagram of project: 
+![Wiring diagram of project](img/mqtt_project_bb.png)
---
+++ MqttProject/pico-mqtt-ex/pico-mqtt-ex.ino
@@ -0,0 +1,119 @@
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#define POT_PIN A0
+#define LED_PIN 5
+
+#define ssid "WIFI_SSID"
+#define password "WIFI_PASS"
+#define mqtt_server "INSTANCE_NAME.cloud.shiftr.io"
+
+#define device_name "pico-dev"
+#define cloud_username "USERNAME"
+#define cloud_password "PASSWORD"
+
+#define potTopic "/" device_name "/pot_val"
+#define ledTopic "/" device_name "/led1"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0, adcVal = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  if (strcmp(ledTopic, topic) == 0)
+  {
+
+    // Switch on the LED if an 1 was received as first character
+    if ((char)payload[0] == '1') {
+      digitalWrite(LED_PIN, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+      // but actually the LED is on; this is because
+      // it is active low on the ESP-01)
+    } else {
+      digitalWrite(LED_PIN, LOW);  // Turn the LED off by making the voltage HIGH
+    }
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(device_name, cloud_username, cloud_password)) {
+      Serial.println("connected");
+      // ... and resubscribe
+      client.subscribe(ledTopic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  //  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(POT_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  adcVal = analogRead(POT_PIN);
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 10000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "{\"cnt\": %d, \"adcVal\": %d}", value, adcVal);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish(potTopic, msg);
+  }
+}
